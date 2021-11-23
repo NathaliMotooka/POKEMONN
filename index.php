cb70fe2a78f5a1e64738631e90833e00d6e3c1fb
NathaliMotooka <94303820+NathaliMotooka@users.noreply.github.com>
2021-11-23 17:46:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<title>Projeto Login</title>
+	<link rel="stylesheet" href="CSS/estilo.css">
+</head>
+<body>
+<div id="corpo-form">
+	<h1>Entrar</h1>
+		<img src="https://picsum.photos/1980/1024" alt="Fixed Background Image" id="background-img"/>
+
+	<form method="POST" action="processa.php">
+		<input type="email" placeholder="Usuário">
+		<input type="password" placeholder="Senha">
+		<input type="submit" value="ACESSAR">
+		<a href="">Ainda não é incrito?<strong>Cadastre-se!</strong></a>
+	</form>
+</div>	
+</body>
+</html>
